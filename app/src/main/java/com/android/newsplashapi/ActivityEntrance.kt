1c9de9b8d47bd6ba78c2e5cb153d75d4b3fefc88
AqibMunir <muniraqib58@gmail.com>
2026-01-09 22:40:43 +0500

    Integrate modern Splash Screen using Android Splash Screen API

---
+++ app/src/main/java/com/android/newsplashapi/ActivityEntrance.kt
@@ -0,0 +1,40 @@
+package com.android.newsplashapi
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ActivityEntrance : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_entrance)
+
+        timeout()
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+
+    private fun timeout() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            navigateScreen()
+        }, 1000)
+    }
+
+    private fun navigateScreen() {
+        startActivity(Intent(this, ActivitySecond::class.java))
+        finish()
+    }
+}
